43ae2b7abcb1045c08f9999e7726d2bc95af6181
Stefania <stefaniagilw@gmail.com>
2025-04-01 23:46:35 -0300

    fix: solucionar problema con la ruta de `globals.css` y la integración de Tailwind

--- nextjs-front/src/app/page.tsx
+++ nextjs-front/src/app/page.tsx
@@ -1,14 +1,6 @@
-import React from "react"
-import Image from "next/image";
-import {Button} from "@heroui/react";
+import { redirect } from "next/navigation"
 
 export default function Home() {
-  return <React.Fragment>
-    <div className={"flex max-w-10"}>
-
-      <Button variant={"shadow"} color={"primary"}>
-        Apretar
-      </Button>
-    </div>
-  </React.Fragment>
+  redirect("/orders")
 }
+
